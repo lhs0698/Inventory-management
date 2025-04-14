0e5160cec5780ffe9445ddd195f145102fb44181
lhs0698 <104623873+lhs0698@users.noreply.github.com>
2025-04-14 20:12:57 +0900

    sidebar 잠시 제거 style 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import React from "react";
 
 import Header from "./components/Header/Header";
 import Products from "./pages/Products/Products";
-import Sidebar from "./components/Sidebar/Sidebar";
+// import Sidebar from "./components/Sidebar/Sidebar";
 
 import "./App.scss";
 
 export default function App() {
   return (
     <div className="app-root">
-      <Sidebar />
+      {/* <Sidebar /> */}
       <div className="main-content">
         <Header />
         <main className="page-content">
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,6 @@ export default function Header() {
   return (
     <header className="app-header">
       <h1 className="logo">ALTA 재고관리</h1>
-      <nav className="nav">
-        <ul>
-          {/* <li><a href="/">홈</a></li>
-          <li><a href="/products">품목</a></li>
-          <li><a href="/settings">설정</a></li> */}
-        </ul>
-      </nav>
     </header>
   )
 }
